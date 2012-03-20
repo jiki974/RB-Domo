b9f369d43a7ff8fba4e419fd4b1531b7521792c5
jiki <jiki@jiki-PC>
2012-03-20 07:16:20 +0100

    just modification

--- tess/src/tsss/client/Tess.java
+++ tess/src/tsss/client/Tess.java
@@ -59,7 +59,7 @@ public class Tess implements EntryPoint {
 
 		// Create the popup dialog box
 		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
+		dialogBox.setText("Remote Procedure Call 2");
 		dialogBox.setAnimationEnabled(true);
 		final Button closeButton = new Button("Close");
 		// We can set the id of a widget by accessing its Element
