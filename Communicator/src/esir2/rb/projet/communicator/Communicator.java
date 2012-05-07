0fca7a34c6e69f0e36177e3082e69e83ec420960
jiki <jiki@jiki-PC.lan>
2012-05-07 19:42:10 +0200

    Update of communicator

--- Communicator/src/esir2/rb/projet/communicator/Communicator.java
+++ Communicator/src/esir2/rb/projet/communicator/Communicator.java
@@ -4,12 +4,19 @@ package esir2.rb.projet.communicator;
 public interface Communicator {
 	
 	public boolean openConnection(String ipadd) throws Exception;
-	public void closeConnection();
-	public void writeOn(int adress_nb) throws Exception;;
-	public void writeOff(int adress_nb) throws Exception;;
-	public boolean readState(int adress_nb) throws Exception;
-	public void allOff() throws Exception;	
-	public boolean isConnected();
+	public void closeConnection(String ipadd);
+	public void writeOn(String ipadd,int adress_nb) throws Exception;;
+	public void writeOff(String ipadd,int adress_nb) throws Exception;;
+	public boolean readState(String ipadd,int adress_nb) throws Exception;
+	public void allOff(String ipadd) throws Exception;	
+	public boolean isConnected(String ipadd);
 	public String getTechno();
+	public void setListener(String ipadd,String gpadd);	
+	public void resetListener(String ipadd,String gpadd);
+	public void removeListener(String ipadd,String gpadd);
+	public boolean isSource(String ipadd,String gpadd);
+	public boolean isDestination(String ipadd,String gpadd);
+	public String getValue(String ipadd,String gpadd);
 
+	
 }
